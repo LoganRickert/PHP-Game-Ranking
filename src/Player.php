efe50cabca72887a6c024d46572513e01c828340
Logan Rickert <rickert.14@wright.edu>
2015-04-02 22:12:09 -0400

    Added initial files

---
+++ src/Player.php
@@ -0,0 +1,29 @@
+<?PHP
+
+class User {
+
+	private $playerId;
+	private $playerName;
+	private $playerEmail;
+	private $playerPassword;
+	private $teamId;
+	private $playerStatus;
+
+	public function __construct($playerId, $playerName, $playerEmail, $playerPassword, $teamId, $playerStatus) {
+		$this->playerId = $playerId;
+		$this->playerName = $playerName;
+		$this->playerEmail = $playerEmail;
+		$this->playerPassword = $playerPassword;
+		$this->teamId = $teamId;
+		$this->playerStatus = $playerStatus;
+	}
+
+	public function getUsername() {
+		return $this->username;
+	}
+
+	public function print() {
+		
+	}
+
+}
---
+++ src/Team.php
@@ -0,0 +1,21 @@
+<?PHP
+
+class User {
+
+	private $teamId;
+	private $teamName;
+	private $teamPoints;
+	private $teamStatus
+
+	public function __construct($teamId, $teamName, $teamPoints, $teamStatus) {
+		$this->teamId = $teamId;
+		$this->teamName = $teamName;
+		$this->teamPoints = $teamPoints;
+		$this->teamStatus = $teamStatus;
+	}
+	
+	public function print() {
+		
+	}
+
+}
