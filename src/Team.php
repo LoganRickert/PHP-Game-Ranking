50576b7230288eb14083330a1765eaeaace5323f
Logan Rickert <rickert.14@wright.edu>
2015-04-04 19:04:09 -0400

    Added getTeamPoints()

--- src/Team.php
+++ src/Team.php
@@ -35,7 +35,7 @@ class Team {
 
 	public function printStats() {
 		$html = new Html("");
-		$html->printTeamStats($this->teamName, $this->teamPoints, $this->teamId);
+		$html->printTeamStats($this->teamName, $this->teamPoints, $this->teamId, $this->teamLeader);
 	}
 
 }
