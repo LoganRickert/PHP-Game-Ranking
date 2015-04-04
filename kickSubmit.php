20a333c0f4b5daeeb6fdbe1dc8d6885f437c3a3c
Logan Rickert <rickert.14@wright.edu>
2015-04-04 19:48:00 -0400

    Now supports team kicking

---
+++ kickSubmit.php
@@ -0,0 +1,39 @@
+<?PHP
+
+include './src/Constants.php';
+include './autoloader.php';
+
+if(!isset($_SESSION['playerId'])) {
+	header("Location: index.php");
+	exit();
+}
+
+$db = new Database();
+
+$teamId = $db->getTeamId(intval($_SESSION['playerId']));
+
+if($teamId == 0 || intval($_REQUEST['playerId']) == intval($_SESSION['playerId']) || !$db->doesPlayerIdExist(intval($_REQUEST['playerId']))) {
+	header("Location: index.php");
+	exit();
+}
+
+if($db->loadTeam($teamId)->getTeamId() != intval($_SESSION['playerId'])) {
+	header("Location: index.php");
+	exit();
+}
+
+if(intval($_REQUEST['playerId']) <= 0) {
+	header("Location: index.php");
+	exit();
+}
+
+$player = $db->loadPlayer(intval($_REQUEST['playerId']));
+
+if($player->getTeamId() != $teamId) {
+	header("Location: index.php");
+	exit();
+}
+
+$db->updateTeamId($player->getTeamId(), 0);
+header("Location: team/".$teamId);
+exit();
