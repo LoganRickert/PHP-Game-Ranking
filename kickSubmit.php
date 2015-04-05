f98a9082a728fdff1af9a9733dda29fc7273660a
Logan Rickert <rickert.14@wright.edu>
2015-04-04 21:52:02 -0400

    Backend support for admin kicking/making leader

--- kickSubmit.php
+++ kickSubmit.php
@@ -11,14 +11,6 @@ if(!isset($_SESSION['playerId'])) {
 
 $db = new Database();
 
-$teamId = $db->getTeamId(intval($_SESSION['playerId']));
-
-// Has no team or is trying to kick theirselves.
-if($teamId == 0 || intval($_REQUEST['playerId']) == intval($_SESSION['playerId'])) {
-	header("Location: index.php");
-	exit();
-}
-
 // Checks to make sure the playerId is valid.
 if(intval($_REQUEST['playerId']) <= 0) {
 	header("Location: index.php");
@@ -31,23 +23,37 @@ if(!$db->doesPlayerIdExist(intval($_REQUEST['playerId']))) {
 	exit();
 }
 
-// Checks to see if they are the team leader.
-if($db->loadTeam($teamId)->getTeamLeader() != intval($_SESSION['playerId'])) {
-	header("Location: index.php");
-	exit();
-}
-
+// Loads the player to kick data.
 $player = $db->loadPlayer(intval($_REQUEST['playerId']));
 
-// Checks to make sure they are on the same team.
-if($player->getTeamId() != $teamId) {
-	header("Location: index.php");
-	exit();
+// If they are an admin, don't check this stuff.
+if(!($db->getGroupId(intval($_SESSION['playerId'])) == ADMIN_GROUP)) {
+
+	// Get the team id for the player trying to do this action.
+	$teamId = $db->getTeamId(intval($_SESSION['playerId']));
+
+	// Has no team or is trying to kick theirselves.
+	if($teamId == 0 || intval($_REQUEST['playerId']) == intval($_SESSION['playerId'])) {
+		header("Location: index.php");
+		exit();
+	}
+
+	// Checks to see if they are the team leader.
+	if($db->loadTeam($teamId)->getTeamLeader() != intval($_SESSION['playerId'])) {
+		header("Location: index.php");
+		exit();
+	}
+
+	// Checks to make sure they are on the same team.
+	if($player->getTeamId() != $teamId) {
+		header("Location: index.php");
+		exit();
+	}
 }
 
 // Change their teamId to 0.
 $db->updateTeamId($player->getPlayerId(), 0);
 
 // Go back to team page.
-header("Location: team/".$teamId);
+header("Location: index.php");
 exit();
