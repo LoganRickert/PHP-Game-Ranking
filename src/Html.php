f9490a484a4a5b1f70fde6fab14c5804d4af7d2a
Logan Rickert <rickert.14@wright.edu>
2015-04-04 21:01:23 -0400

    Fixed error if not signed in

--- src/Html.php
+++ src/Html.php
@@ -144,7 +144,7 @@ class Html {
 			} else {
 				echo "<li><a href=\"" . $this->fullSiteRoot . "/player/" . $player->getPlayerId() . "\">" . $player->getPlayerName() . "</a>";
 
-				if($teamLeader == $_SESSION['playerId']) {
+				if(isset($_SESSION['playerId']) && $teamLeader == $_SESSION['playerId']) {
 					echo " - <a href=\"" . $this->fullSiteRoot . "/kickSubmit.php?playerId=" . $player->getPlayerId() . "\">Kick</a> 
 					&middot; <a href=\"" . $this->fullSiteRoot . "/makeLeaderSubmit.php?playerId=" . $player->getPlayerId() . "\">Make Leader</a></li>";
 				} else {
